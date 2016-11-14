a76b915feb4e47f0f609ec6170586a1af2a24090
Artem <wheelsandbytes@gmail.com>
2016-11-14 17:13:49 -0500

    sendMove method in progress

--- src/Player.java
+++ src/Player.java
@@ -12,9 +12,14 @@ public abstract class Player {
 	// SEND MOVE
 	// Player sends a move object
 	// Move object contains a coordinate, a rotation, and a tile
-	public void sendMove(Move m);
+	public Move sendMove(Coor c, Rot r, Tile t);
 	{
 		// method call to send the move to the Board
+		// set the coordinate, rotation first
+		// package the tile to be sent into a move object
+		// Return a Move object for the TCP adapter to grab
+		Move m = new Move(c,r,t);
+		return m;
 	}
 
 	//Any changes to a Players score has to go through this setter
