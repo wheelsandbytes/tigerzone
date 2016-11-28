c45b071f39de5da8fb0df7cb2717d5dc73afde75
sadelarosa <sadelarosa@ufl.edu>
2016-11-28 04:58:52 -0500

    ai fixed
    ai fixed

--- src/AI.java
+++ src/AI.java
@@ -33,8 +33,8 @@ public class AI extends Player{
 	 }
 
 	@Override
-	public Move decideMove(Tile t) {
-	 	List<Move> possibleMoves = mainBoard.find(t);
+	public Move decideMove() {
+	 	List<Move> possibleMoves = mainBoard.find(globalDeck.getCurrent());
 	 	Random rand = new Random();
 	 	int randomInt = rand.nextInt(possibleMoves.size());
 		// TODO Auto-generated method stub
